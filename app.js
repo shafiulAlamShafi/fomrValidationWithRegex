33d1db74bc6a6cb9caba7bf2f505ebe1d5d22c73
shafiulAlamShafi <shafiicubd@gmail.com>
2023-08-19 00:55:12 +0600

    regex addition
    all regular expression patterns are added for input fields

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+const inputs = document.forms["userLogin"].querySelectorAll("input");
+const patterns = {
+  telephone: /^\d{11}$/,
+  username: /^[a-z\d]{5,12}$/i,
+  password: /^[\w@-]{8,12}$/,
+  slug: /^[a-z\d-]{8,20}$/,
+  email: /^([a-z\d\.*-?]+)@([a-z\d-?]+)\.([a-z]{2,5})(\.[a-z]{2,5})?$/,
+};
 
+function validate(inputElement, regex) {
+  if (regex.test(inputElement.value)) {
+    inputElement.className = "valid";
+    inputElement.nextElementSibling.style.display = "none";
+  } else {
+    inputElement.className = "invalid";
+    inputElement.nextElementSibling.style.display = "block";
+  }
+}
+
+inputs.forEach((element) => {
+  element.addEventListener("keyup", (e) => {
+    validate(e.target, patterns[e.target.name]);
+  });
+});
